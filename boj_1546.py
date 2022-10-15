ff50793bea65664902c48664eebf62bb6f0e6350
Park <gylee206@gmail.com>
2022-10-15 16:01:48 +0900

    add -> boj_1546 (solved)

---
+++ boj_1546.py
@@ -0,0 +1,5 @@
+n_scores = int(input())
+scores = list(map(int, input().split()))
+max_score = max(scores)
+final_scores = list(map(lambda x:x/max_score*100, scores))
+print(sum(final_scores) / len(final_scores))
